cceaac15fc01508d185673c291dabb8d3bc80881
khokhanptv <thaitnpy92@gmail.com>
2023-07-06 17:57:12 +0700

    ver 1.0

---
+++ BT_TUAN4/MDK-ARM/BT_TUAN4/retarget_io.d
@@ -0,0 +1,9 @@
+bt_tuan4/retarget_io.o: \
+  C:\Users\ADMIN\AppData\Local\Arm\Packs\Keil\ARM_Compiler\1.7.2\Source\retarget_io.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\assert.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\rt_sys.h \
+  RTE\_BT_TUAN4\RTE_Components.h
---
+++ CONTROL_LED/MDK-ARM/CONTROL_LED/retarget_io.d
@@ -0,0 +1,9 @@
+control_led/retarget_io.o: \
+  C:\Users\ADMIN\AppData\Local\Arm\Packs\Keil\ARM_Compiler\1.7.2\Source\retarget_io.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\assert.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\rt_sys.h \
+  RTE\_CONTROL_LED\RTE_Components.h
