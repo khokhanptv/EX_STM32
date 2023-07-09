474180e791c24ecac98fc58ff6a0adf2738ac534
khokhanptv <thaitnpy92@gmail.com>
2023-07-09 19:07:15 +0700

    ver 3.0

---
+++ STM32_ROLE_PLUS/MDK-ARM/STM32_ROLE_PLUS/retarget_io.d
@@ -0,0 +1,9 @@
+stm32_role_plus/retarget_io.o: \
+  C:\Users\khokh\AppData\Local\Arm\Packs\Keil\ARM_Compiler\1.7.2\Source\retarget_io.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\assert.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\rt_sys.h \
+  RTE\_STM32_ROLE_PLUS\RTE_Components.h
